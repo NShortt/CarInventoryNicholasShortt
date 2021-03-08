8d9a59936b125580374b1e1fe3868e05796f9495
Nicholas Shortt <Nicholas.Shortt@hotmail.com>
2021-03-08 11:18:09 -0500

    Multiple changes to the form design

--- Form.Designer.cs
+++ Form.Designer.cs
@@ -29,6 +29,7 @@ namespace CarInventoryNicholasShortt
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.checkBoxNew = new System.Windows.Forms.CheckBox();
             this.textBoxLastName = new System.Windows.Forms.TextBox();
             this.labelPrice = new System.Windows.Forms.Label();
@@ -38,11 +39,18 @@ namespace CarInventoryNicholasShortt
             this.labelYear = new System.Windows.Forms.Label();
             this.labelMake = new System.Windows.Forms.Label();
             this.comboBoxModel = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
             this.buttonExit = new System.Windows.Forms.Button();
             this.buttonReset = new System.Windows.Forms.Button();
             this.buttonEnter = new System.Windows.Forms.Button();
             this.listViewEntries = new System.Windows.Forms.ListView();
+            this.columnHeaderNew = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderID = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderMake = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderModel = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderYear = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderPrice = new System.Windows.Forms.ColumnHeader();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // checkBoxNew
@@ -54,7 +62,7 @@ namespace CarInventoryNicholasShortt
             this.checkBoxNew.Margin = new System.Windows.Forms.Padding(4);
             this.checkBoxNew.Name = "checkBoxNew";
             this.checkBoxNew.Size = new System.Drawing.Size(86, 34);
-            this.checkBoxNew.TabIndex = 9;
+            this.checkBoxNew.TabIndex = 8;
             this.checkBoxNew.Text = "&New:";
             this.checkBoxNew.UseVisualStyleBackColor = true;
             // 
@@ -65,7 +73,8 @@ namespace CarInventoryNicholasShortt
             this.textBoxLastName.Name = "textBoxLastName";
             this.textBoxLastName.ReadOnly = true;
             this.textBoxLastName.Size = new System.Drawing.Size(223, 35);
-            this.textBoxLastName.TabIndex = 8;
+            this.textBoxLastName.TabIndex = 7;
+            this.toolTip.SetToolTip(this.textBoxLastName, "Enter the price of the car.");
             // 
             // labelPrice
             // 
@@ -73,7 +82,7 @@ namespace CarInventoryNicholasShortt
             this.labelPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelPrice.Name = "labelPrice";
             this.labelPrice.Size = new System.Drawing.Size(131, 30);
-            this.labelPrice.TabIndex = 7;
+            this.labelPrice.TabIndex = 6;
             this.labelPrice.Text = "&Price:";
             this.labelPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
@@ -83,7 +92,7 @@ namespace CarInventoryNicholasShortt
             this.labelModel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelModel.Name = "labelModel";
             this.labelModel.Size = new System.Drawing.Size(131, 30);
-            this.labelModel.TabIndex = 3;
+            this.labelModel.TabIndex = 2;
             this.labelModel.Text = "M&odel:";
             this.labelModel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
@@ -94,7 +103,8 @@ namespace CarInventoryNicholasShortt
             this.textBoxFirstName.Name = "textBoxFirstName";
             this.textBoxFirstName.ReadOnly = true;
             this.textBoxFirstName.Size = new System.Drawing.Size(223, 35);
-            this.textBoxFirstName.TabIndex = 4;
+            this.textBoxFirstName.TabIndex = 3;
+            this.toolTip.SetToolTip(this.textBoxFirstName, "Enter the car\'s model.");
             // 
             // comboBox1
             // 
@@ -118,7 +128,8 @@ namespace CarInventoryNicholasShortt
             this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(224, 38);
-            this.comboBox1.TabIndex = 19;
+            this.comboBox1.TabIndex = 5;
+            this.toolTip.SetToolTip(this.comboBox1, "Select the year the car was made.");
             // 
             // labelYear
             // 
@@ -126,7 +137,7 @@ namespace CarInventoryNicholasShortt
             this.labelYear.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelYear.Name = "labelYear";
             this.labelYear.Size = new System.Drawing.Size(131, 30);
-            this.labelYear.TabIndex = 5;
+            this.labelYear.TabIndex = 4;
             this.labelYear.Text = "&Year:";
             this.labelYear.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
@@ -136,7 +147,7 @@ namespace CarInventoryNicholasShortt
             this.labelMake.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelMake.Name = "labelMake";
             this.labelMake.Size = new System.Drawing.Size(131, 30);
-            this.labelMake.TabIndex = 1;
+            this.labelMake.TabIndex = 0;
             this.labelMake.Text = "&Make:";
             this.labelMake.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
@@ -161,63 +172,105 @@ namespace CarInventoryNicholasShortt
             this.comboBoxModel.Margin = new System.Windows.Forms.Padding(4);
             this.comboBoxModel.Name = "comboBoxModel";
             this.comboBoxModel.Size = new System.Drawing.Size(224, 38);
-            this.comboBoxModel.TabIndex = 2;
+            this.comboBoxModel.TabIndex = 1;
+            this.toolTip.SetToolTip(this.comboBoxModel, "Select the car\'s make.");
             // 
-            // label2
+            // labelResult
             // 
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label2.Location = new System.Drawing.Point(21, 448);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(546, 52);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "label2";
+            this.labelResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelResult.Location = new System.Drawing.Point(21, 526);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(546, 88);
+            this.labelResult.TabIndex = 10;
+            this.toolTip.SetToolTip(this.labelResult, "Display message about attempt to add to list.");
             // 
             // buttonExit
             // 
-            this.buttonExit.Location = new System.Drawing.Point(436, 503);
+            this.buttonExit.Location = new System.Drawing.Point(436, 626);
             this.buttonExit.Name = "buttonExit";
             this.buttonExit.Size = new System.Drawing.Size(131, 40);
-            this.buttonExit.TabIndex = 14;
+            this.buttonExit.TabIndex = 13;
             this.buttonExit.Text = "E&xit";
+            this.toolTip.SetToolTip(this.buttonExit, "Click to close the form.");
             this.buttonExit.UseVisualStyleBackColor = true;
             // 
             // buttonReset
             // 
-            this.buttonReset.Location = new System.Drawing.Point(299, 503);
+            this.buttonReset.Location = new System.Drawing.Point(299, 626);
             this.buttonReset.Name = "buttonReset";
             this.buttonReset.Size = new System.Drawing.Size(131, 40);
-            this.buttonReset.TabIndex = 13;
+            this.buttonReset.TabIndex = 12;
             this.buttonReset.Text = "&Reset";
+            this.toolTip.SetToolTip(this.buttonReset, "Click to clear entry fields.");
             this.buttonReset.UseVisualStyleBackColor = true;
             // 
             // buttonEnter
             // 
-            this.buttonEnter.Location = new System.Drawing.Point(162, 503);
+            this.buttonEnter.Location = new System.Drawing.Point(162, 626);
             this.buttonEnter.Name = "buttonEnter";
             this.buttonEnter.Size = new System.Drawing.Size(131, 40);
-            this.buttonEnter.TabIndex = 12;
+            this.buttonEnter.TabIndex = 11;
             this.buttonEnter.Text = "&Enter";
+            this.toolTip.SetToolTip(this.buttonEnter, "Click to enter data to the list.");
             this.buttonEnter.UseVisualStyleBackColor = true;
             // 
             // listViewEntries
             // 
+            this.listViewEntries.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderNew,
+            this.columnHeaderID,
+            this.columnHeaderMake,
+            this.columnHeaderModel,
+            this.columnHeaderYear,
+            this.columnHeaderPrice});
             this.listViewEntries.HideSelection = false;
             this.listViewEntries.Location = new System.Drawing.Point(21, 275);
+            this.listViewEntries.MultiSelect = false;
             this.listViewEntries.Name = "listViewEntries";
-            this.listViewEntries.Size = new System.Drawing.Size(546, 170);
-            this.listViewEntries.TabIndex = 10;
+            this.listViewEntries.Size = new System.Drawing.Size(546, 241);
+            this.listViewEntries.TabIndex = 9;
+            this.toolTip.SetToolTip(this.listViewEntries, "Display lists of entered data.");
             this.listViewEntries.UseCompatibleStateImageBehavior = false;
+            this.listViewEntries.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderNew
+            // 
+            this.columnHeaderNew.Text = "New";
+            // 
+            // columnHeaderID
+            // 
+            this.columnHeaderID.Text = "ID";
+            // 
+            // columnHeaderMake
+            // 
+            this.columnHeaderMake.Text = "Make";
+            this.columnHeaderMake.Width = 120;
+            // 
+            // columnHeaderModel
+            // 
+            this.columnHeaderModel.Text = "Model";
+            this.columnHeaderModel.Width = 120;
+            // 
+            // columnHeaderYear
+            // 
+            this.columnHeaderYear.Text = "Year";
+            this.columnHeaderYear.Width = 80;
+            // 
+            // columnHeaderPrice
+            // 
+            this.columnHeaderPrice.Text = "Price";
+            this.columnHeaderPrice.Width = 100;
             // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(598, 559);
+            this.ClientSize = new System.Drawing.Size(598, 678);
             this.Controls.Add(this.listViewEntries);
             this.Controls.Add(this.buttonEnter);
             this.Controls.Add(this.buttonReset);
             this.Controls.Add(this.buttonExit);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelResult);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.labelYear);
             this.Controls.Add(this.comboBoxModel);
@@ -228,6 +281,7 @@ namespace CarInventoryNicholasShortt
             this.Controls.Add(this.labelMake);
             this.Controls.Add(this.labelModel);
             this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -244,11 +298,18 @@ namespace CarInventoryNicholasShortt
         private System.Windows.Forms.Label labelYear;
         private System.Windows.Forms.Label labelMake;
         private System.Windows.Forms.ComboBox comboBoxModel;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelResult;
         private System.Windows.Forms.Button buttonExit;
         private System.Windows.Forms.Button buttonReset;
         private System.Windows.Forms.Button buttonEnter;
         private System.Windows.Forms.ListView listViewEntries;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ColumnHeader columnHeaderNew;
+        private System.Windows.Forms.ColumnHeader columnHeaderID;
+        private System.Windows.Forms.ColumnHeader columnHeaderMake;
+        private System.Windows.Forms.ColumnHeader columnHeaderModel;
+        private System.Windows.Forms.ColumnHeader columnHeaderYear;
+        private System.Windows.Forms.ColumnHeader columnHeaderPrice;
     }
 }
 
