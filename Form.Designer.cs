bd9080c6fd7f53d8f66fcd41d5adc2f67cb21d87
Nicholas Shortt <Nicholas.Shortt@hotmail.com>
2021-03-08 10:59:30 -0500

    Add project files.

---
+++ Form.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace CarInventoryNicholasShortt
+{
+    partial class Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxNew = new System.Windows.Forms.CheckBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.labelModel = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelYear = new System.Windows.Forms.Label();
+            this.labelMake = new System.Windows.Forms.Label();
+            this.comboBoxModel = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.listViewEntries = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // checkBoxNew
+            // 
+            this.checkBoxNew.AutoSize = true;
+            this.checkBoxNew.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBoxNew.Enabled = false;
+            this.checkBoxNew.Location = new System.Drawing.Point(101, 234);
+            this.checkBoxNew.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBoxNew.Name = "checkBoxNew";
+            this.checkBoxNew.Size = new System.Drawing.Size(86, 34);
+            this.checkBoxNew.TabIndex = 9;
+            this.checkBoxNew.Text = "&New:";
+            this.checkBoxNew.UseVisualStyleBackColor = true;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(160, 178);
+            this.textBoxLastName.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.ReadOnly = true;
+            this.textBoxLastName.Size = new System.Drawing.Size(223, 35);
+            this.textBoxLastName.TabIndex = 8;
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.Location = new System.Drawing.Point(21, 178);
+            this.labelPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(131, 30);
+            this.labelPrice.TabIndex = 7;
+            this.labelPrice.Text = "&Price:";
+            this.labelPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelModel
+            // 
+            this.labelModel.Location = new System.Drawing.Point(21, 69);
+            this.labelModel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelModel.Name = "labelModel";
+            this.labelModel.Size = new System.Drawing.Size(131, 30);
+            this.labelModel.TabIndex = 3;
+            this.labelModel.Text = "M&odel:";
+            this.labelModel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(160, 69);
+            this.textBoxFirstName.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.ReadOnly = true;
+            this.textBoxFirstName.Size = new System.Drawing.Size(223, 35);
+            this.textBoxFirstName.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 30;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Dr.",
+            "Duchess",
+            "Duke",
+            "Hon.",
+            "Lady",
+            "Lord",
+            "Mr.",
+            "Mrs.",
+            "Ms.",
+            "Mx.",
+            "Sir"});
+            this.comboBox1.Location = new System.Drawing.Point(160, 122);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(224, 38);
+            this.comboBox1.TabIndex = 19;
+            // 
+            // labelYear
+            // 
+            this.labelYear.Location = new System.Drawing.Point(21, 125);
+            this.labelYear.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelYear.Name = "labelYear";
+            this.labelYear.Size = new System.Drawing.Size(131, 30);
+            this.labelYear.TabIndex = 5;
+            this.labelYear.Text = "&Year:";
+            this.labelYear.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelMake
+            // 
+            this.labelMake.Location = new System.Drawing.Point(21, 16);
+            this.labelMake.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelMake.Name = "labelMake";
+            this.labelMake.Size = new System.Drawing.Size(131, 30);
+            this.labelMake.TabIndex = 1;
+            this.labelMake.Text = "&Make:";
+            this.labelMake.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // comboBoxModel
+            // 
+            this.comboBoxModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxModel.Enabled = false;
+            this.comboBoxModel.FormattingEnabled = true;
+            this.comboBoxModel.Items.AddRange(new object[] {
+            "Dr.",
+            "Duchess",
+            "Duke",
+            "Hon.",
+            "Lady",
+            "Lord",
+            "Mr.",
+            "Mrs.",
+            "Ms.",
+            "Mx.",
+            "Sir"});
+            this.comboBoxModel.Location = new System.Drawing.Point(160, 13);
+            this.comboBoxModel.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxModel.Name = "comboBoxModel";
+            this.comboBoxModel.Size = new System.Drawing.Size(224, 38);
+            this.comboBoxModel.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(21, 448);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(546, 52);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(436, 503);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(131, 40);
+            this.buttonExit.TabIndex = 14;
+            this.buttonExit.Text = "E&xit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(299, 503);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(131, 40);
+            this.buttonReset.TabIndex = 13;
+            this.buttonReset.Text = "&Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(162, 503);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(131, 40);
+            this.buttonEnter.TabIndex = 12;
+            this.buttonEnter.Text = "&Enter";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            // 
+            // listViewEntries
+            // 
+            this.listViewEntries.HideSelection = false;
+            this.listViewEntries.Location = new System.Drawing.Point(21, 275);
+            this.listViewEntries.Name = "listViewEntries";
+            this.listViewEntries.Size = new System.Drawing.Size(546, 170);
+            this.listViewEntries.TabIndex = 10;
+            this.listViewEntries.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 559);
+            this.Controls.Add(this.listViewEntries);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelYear);
+            this.Controls.Add(this.comboBoxModel);
+            this.Controls.Add(this.checkBoxNew);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.labelMake);
+            this.Controls.Add(this.labelModel);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkBoxNew;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.Label labelModel;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelYear;
+        private System.Windows.Forms.Label labelMake;
+        private System.Windows.Forms.ComboBox comboBoxModel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.ListView listViewEntries;
+    }
+}
+
